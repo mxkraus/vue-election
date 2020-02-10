b810b68e972f69ad65c3783d47c5eb0a7495dbc3
Maximilian Kraus <maximiliankraus@macbook-mxkraus.fritz.box>
2020-02-10 22:19:24 +0100

    Stimmenvergabe Listenkreuz

--- src/components/Election.vue
+++ src/components/Election.vue
@@ -3,25 +3,23 @@
       
     <div class="row listenkreuz">
         <div class="holder">
+
+        </div>
+        <div class="holder">
             <button class="votefield" @click='toggleCross'>
                 <div class="cross" v-if="showCross" ref="cross">X</div>
             </button>
         </div>
         <div class="liner">
-            <p>
-                Sie haben noch {{ total }} Stimmen!
-            </p>
-            <p>
-                Gehäufelte Stimmen: {{ accumulated }}
-            </p>
+            <span>Sie haben noch {{ total }} Stimmen. <br> Gehäufelte Stimmen: {{ accumulated }}</span>
         </div>
     </div>
 
     <div class="row" v-for="(item, index) in this.candidates" v-bind:key="index">
+        <div class="holder" >
+            <span>00{{item}}</span>
+        </div>
         <div class="holder">
-            <div class="nr" >
-                <p>00{{item}}</p>
-            </div>
             <input type="text" maxlength="1" ref="candidate" class="votefield" @change="handleVoting(index, $event)"/>
         </div>
         <div class="liner"></div>
@@ -40,6 +38,7 @@
                 accumulated: 0,
                 previousVote: 0,
                 showCross: false,
+                allCandidates: [],
                 candidatesWithAccumulation: [],
                 candidatesWithVoting: [],
                 candidateVote: "" // hält die Stimmen, die der Kandidat bekommen hat
@@ -53,23 +52,18 @@
         updated() {
 
             // Alle Kandidaten sammeln
-            let allCandidates = this.$refs.candidate;
+            this.allCandidates = this.$refs.candidate;
 
-            /**
-             * Hier muss der Array zurückgesetzt werden, sonst zähl er das neue daszu
-             */
-            this.candidatesWithAccumulation = [];
-            allCandidates.forEach((element, index) => {
-                if(element.value > 1){
-                    this.candidatesWithAccumulation.push(index);
-                }
-            });
+            // Häufelung zurückesetzen wenn mehr S
+            if(this.accumulated < 0){
+                this.accumulated = 0;
+            }
 
             /**
              * Alle Kandidaten sammlen, die irgend eine Stimme erhalten haben
              */
             this.candidatesWithVoting = [];
-            allCandidates.forEach((element, index) => {
+            this.allCandidates.forEach((element, index) => {
                 if(element.value >= 1){
                     let candi = {
                         idx: index,
@@ -83,48 +77,82 @@
         methods: {
             handleVoting(index, event){
 
-                let assignedVote = event.srcElement.value; // Eingegebener Wert
+                let aV = parseInt(event.srcElement.value); // Eingegebener Wert
+                let assignedVote = Number.isNaN(aV) ? '' : aV;
 
-                if( assignedVote == '' ){
-                    try{
-                        this.total += parseInt(this.candidatesWithVoting.find( c => c.idx == index ).vote);
-                    }catch(err){
-                        console.log(err);
+                if( assignedVote > 3 ){ // nur max. 3 Stimmen gültig
+                    try {
+                        // altes Voting wiederherstellen
+                        event.srcElement.value = this.candidatesWithVoting[index].vote;
+                    } catch(err) {
+                        event.srcElement.value = '';
                     }
+                    alert("Bitte nicht mehr als 3 Stimmen je Kandidat vergeben!")
                     return;
                 }
 
-                if( this.total > 0 ){ // Noch Stimmen verfügbar
+                try{
+                    // Altes Voting mit index aus Array lesen
+                    let prevVoting = parseInt(this.candidatesWithVoting.find( c => c.idx == index ).vote);
+                    let diff = 0;
+
+                    if( assignedVote == '' ){
+                        this.total += prevVoting;
+                        this.accumulated -= prevVoting;
+                        return;
+                    }else if( assignedVote < prevVoting ){
+                        diff = prevVoting - assignedVote;
+                        this.total += diff;
+                        this.accumulated -= assignedVote == 1 ? prevVoting : diff;
+                        return;
+                    }else if( assignedVote > prevVoting ){
+                        diff = assignedVote - prevVoting;
+                        // Nur abziehen wenn eh nicht schon alle Stimmen weg sind
+                        this.total -= this.showCross == false ? diff : 0; 
+                        this.accumulated += prevVoting == 1 ? assignedVote : diff;
+
+                        if(this.showCross == true){
+
+                            this.candidatesWithAccumulation = [];
+                            this.allCandidates.forEach((element, index) => {
+                                if(element.value > 1){
+                                    this.candidatesWithAccumulation.push(index);
+                                }
+                            });
+                        
+                            let until = this.accumulated - this.candidatesWithAccumulation.length;
+                            for(let i=this.candidates; i>this.candidates - until; i--){
+                                this.$refs.candidate[i-1].value = '';
+                            }
+                        }
+
+                        return;
+                    }
+
+                }catch(err){
+                    console.log(err);
+                }
+
+
+                if( this.total > 0 ){ 
 
                     // Stimmen wurden gehäufelt
                     if( assignedVote > 1 && assignedVote <= 3 ){
-
                         this.accumulated += parseInt(assignedVote);
-
-                    }else if( assignedVote > 3 ){
-
-                        try {
-                            event.srcElement.value = this.candidatesWithVoting[index].vote;
-                        } catch(err) {
-                            event.srcElement.value = '';
-                        }
-
-                        alert("Bitte nicht mehr als 3 Stimmen je Kandidat vergeben!")
-                        return;
-
                     }
 
                     // Angegebene Stimmen von Gesamtstimmen abziehen
                     this.total -= assignedVote;
 
                 }else{ // Keine Stimmen mehr verfügbar
-
-                    event.srcElement.value = '';
-                    alert("Alle Stimmen bereits vergeben!");
-                    return; 
-
+                    if( this.showCross == false){
+                        event.srcElement.value = '';
+                        alert("Alle Stimmen bereits vergeben!");
+                        return; 
+                    }
                 }
                 
+
             },
             toggleCross(){
 
@@ -161,7 +189,6 @@
 
             }
 
-
         }
         
     }
@@ -177,19 +204,30 @@
         display: table;
         border-collapse: collapse;
 
+        .listenkreuz{
+
+            .liner{
+                height: 80px;
+                span{
+                    position: relative;
+                    bottom: -16px;
+                }
+            }
+        }
+
         .votefield{
             width: 40px;
             height: 40px;
             display: block;
-            background-color: aliceblue;
+            background-color: #9dafbe;
             border-radius: 40px;
             margin: 0 auto;
             line-height: 50px;
             position: relative;
-            top: -14px;
             border: none;
             text-align: center;
-            font-size: 2em;
+            font-size: 20px;
+            margin: 5px;
 
             &:active,
             &:focus{
@@ -202,10 +240,9 @@
             font-size: 30px;
             font-weight: bold;
             color: blue;
-            position: absolute;
-            z-index: 1;
-            top: -6px;
-            left: 8px;
+            position: relative;
+            top: -2px;
+            left: 1px;
         }
         
         .row{
@@ -214,25 +251,20 @@
             height: 50px;
             width: 100%;
             position: relative;
-            
-            
         }
 
         .liner{
-                display: table-cell;
-                border: inherit;
-                width: 80%;
-                height: 50px;
-            }
+            display: table-cell;
+            border: inherit;
+            height: 50px;
+        }
 
         .holder{
             display: table-cell;
-            border: inherit;
+            //border: inherit;
             width: 10%;
             height: 50px;
-        
-            
-            
+            vertical-align: middle;
         }
     }
     
